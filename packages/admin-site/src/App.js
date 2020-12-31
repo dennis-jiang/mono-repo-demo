467fd5cbffc42b83a95f50e2d934c04a2da17341
Jiang, Dennis <>
2020-12-31 16:27:56 +0800

    Adjust structure

--- packages/admin-site/src/App.js
+++ packages/admin-site/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import WaterForm from '@mono-repo-demo/common/components/WaterForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>管理员站点</h2>
+      <WaterForm />
     </div>
   );
 }
